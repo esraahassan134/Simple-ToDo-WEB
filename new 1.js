5b706eb7448ed03d62dd1ae9eff52bdc977a4244
Esraa Hassan <esraahassan134@gmail.com>
2025-10-06 19:33:13 +0300

    Initial commit

---
+++ new 1.js
@@ -0,0 +1,21 @@
+let addBtn = document.querySelector("#addBtn");
+let taskInput = document.querySelector("#taskInput");
+let taskList = document.querySelector("#taskList");
+
+addBtn.addEventListener("click", function() {
+  let taskText = taskInput.value;
+
+  if (taskText === "") {
+    alert("Please enter a task!");
+  } else {
+    let li = document.createElement("li");
+    li.textContent = taskText;
+
+    li.addEventListener("click", function() {
+      li.classList.toggle("done");
+    });
+
+    taskList.appendChild(li);
+    taskInput.value = ""; // يمسح النص بعد الإضافة
+  }
+});
